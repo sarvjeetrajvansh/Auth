44b078e455ee9d7c55bf377d58a2083a1f23885e
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2023-10-29 00:35:11 +0530

    Feat : Added Spring security

---
+++ src/main/java/com/example/auth/controllers/AuthController.java
@@ -0,0 +1,14 @@
+package com.example.auth.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/")
+    public String Hello(){
+
+        return "Hello";
+    }
+}
